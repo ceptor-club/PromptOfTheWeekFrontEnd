c4718e868cd916eb80f13c8248449d7193b95789
danj-O <danjo.develop@gmail.com>
2023-02-20 21:04:04 -0500

    test commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  //adding stuff to push to check on vercel
+  res.status(200).json({ name: "John Doe" })
 }
