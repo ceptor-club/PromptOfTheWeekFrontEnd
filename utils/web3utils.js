0c24e6e8413458a9bcf40983b7713cbb373a1d65
tippi fifestarr <tippi@boober.org>
2023-02-28 21:51:23 -0600

    fixed NFT bug by hardcoding some pdfStats, adding missing env, new bug image render?

--- utils/web3utils.js
+++ utils/web3utils.js
@@ -1,22 +1,26 @@
-import { NFTStorage, File } from "nft.storage"
+import { NFTStorage, File } from "nft.storage";
 
-const token = process.env.NEXT_PUBLIC_NFT_STORAGE
+const token = process.env.NEXT_PUBLIC_NFT_STORAGE;
 
 // takes in base64 binary image data
 async function avatarNFTSTORAGE(someBinaryImageData, prompt, pdfData) {
-  const client = new NFTStorage({ token: token })
-  const uploadObject = await createStructuredMetadata(pdfData, prompt, someBinaryImageData)
-  const metadata = await client.store(uploadObject)
-  return metadata.url
+  const client = new NFTStorage({ token: token });
+  const uploadObject = await createStructuredMetadata(
+    pdfData,
+    prompt,
+    someBinaryImageData
+  );
+  const metadata = await client.store(uploadObject);
+  return metadata.url;
 }
 
 const base64EncodedImage = async (base64ImageString) => {
-  const fetchImage = await fetch(base64ImageString)
-  console.log("fetchImage", fetchImage)
-  const blob = await fetchImage.blob()
-  const file = new File([blob], "something.png", { type: "image/png" })
-  return file
-}
+  const fetchImage = await fetch(base64ImageString);
+  console.log("fetchImage", fetchImage);
+  const blob = await fetchImage.blob();
+  const file = new File([blob], "something.png", { type: "image/png" });
+  return file;
+};
 
 async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
   // const imageFile = new File([someBinaryImageData], "diffused.png", {
@@ -27,9 +31,9 @@ async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
   // const blob = new Blob([someBinaryImageData], { type: "image/png" })
   // console.log("imageFile", blob)
 
-  const imageFile = await base64EncodedImage(someBinaryImageData)
+  const imageFile = await base64EncodedImage(someBinaryImageData);
 
-  console.log("imageFile", imageFile)
+  console.log("imageFile", imageFile);
 
   const structuredMetadata = {
     name: "Wizard NFT!",
@@ -57,27 +61,27 @@ async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
       },
       {
         trait_type: "Strength",
-        value: pdfData.str[1],
+        value: 5,
       },
       {
         trait_type: "Dexterity",
-        value: pdfData.dex[1],
+        value: 5,
       },
       {
         trait_type: "Constitution",
-        value: pdfData.con[1],
+        value: 5,
       },
       {
         trait_type: "Intelligence",
-        value: pdfData.int[1],
+        value: 5,
       },
       {
         trait_type: "Wisdom",
-        value: pdfData.wis[1],
+        value: 5,
       },
       {
         trait_type: "Charisma",
-        value: pdfData.cha[1],
+        value: 5,
       },
       {
         value: pdfData.playerName ? pdfData.playerName : pdfData.name,
@@ -87,9 +91,9 @@ async function createStructuredMetadata(pdfData, prompt, someBinaryImageData) {
         value: pdfData.background,
       },
     ],
-  }
+  };
 
-  return structuredMetadata
+  return structuredMetadata;
 }
 
-module.exports = { avatarNFTSTORAGE }
+module.exports = { avatarNFTSTORAGE };
