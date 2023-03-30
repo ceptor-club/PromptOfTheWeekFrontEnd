b350e3c2e4fff5b774f219ae9a00db95608eb3fd
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-03-30 00:46:49 -0400

    frame for a modal after successful minting

--- components/GenerateButton.jsx
+++ components/GenerateButton.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
-import Image from "next/image";
-import { CONSTANTS } from "../utils/CONSTANTS";
-import { useRouter } from "next/router";
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { CONSTANTS } from '../utils/CONSTANTS';
+import { useRouter } from 'next/router';
 
 const GenerateButton = ({
   setConditionalCreate,
@@ -18,33 +18,33 @@ const GenerateButton = ({
   // Attempting to scroll to the results section whenever imageResult is updated
   useEffect(() => {
     if (imageResult) {
-      const resultSection = document.getElementById("results");
-      resultSection.scrollIntoView({ behavior: "smooth" });
+      const resultSection = document.getElementById('results');
+      resultSection.scrollIntoView({ behavior: 'smooth' });
     }
   }, [imageResult]);
 
   const generateImages = async () => {
-    console.log("Generating images... for ", prompt);
+    console.log('Generating images... for ', prompt);
     setError(false);
     setImageProcessing(true);
-    setConditionalCreate("results");
-    const fetchResult = await fetch("/api/getImage", {
+    setConditionalCreate('results');
+    const fetchResult = await fetch('/api/getImage', {
       // <------------- COMMENTED OUT FOR TESTING
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
       body: JSON.stringify({
         data: prompt,
       }),
     }); //result is given as base64 encoded images
-    const result = await fetchResult.json();
+    //const result = await fetchResult.json();
 
-    // const result = { images: [CONSTANTS.testBase64Image] }; // <------------- THIS IS FOR TESTING
+    const result = { images: [CONSTANTS.testBase64Image] }; // <------------- THIS IS FOR TESTING
 
-    console.log("result: ", result);
-    console.log("res prompt:", result.prompted);
+    console.log('result: ', result);
+    console.log('res prompt:', result.prompted);
     setImageProcessing(false);
     if (result.error) {
       return setError(result.error);
@@ -55,62 +55,62 @@ const GenerateButton = ({
   return (
     <>
       <div
-        className="relative flex justify-center text-center align-center mt-6 cursor-pointer w-96 h-28 m-4"
+        className='relative flex justify-center text-center align-center mt-6 cursor-pointer w-96 h-28 m-4'
         onClick={generateImages}
         disabled={pdfData ? false : true}
       >
         <Image
-          src="/images/Buttons/generateBackground2.png"
-          alt=""
+          src='/images/Buttons/generateBackground2.png'
+          alt=''
           width={200}
           height={120}
-          className="absolute my-auto w-full h-full bottom-[2px] left-[2px] -z-20"
+          className='absolute my-auto w-full h-full bottom-[2px] left-[2px] -z-20'
         />
         <Image
           src={
             imageResult
-              ? "/images/Buttons/rerollbackground.png"
-              : "/images/Buttons/generateBackground.png"
+              ? '/images/Buttons/rerollbackground.png'
+              : '/images/Buttons/generateBackground.png'
           }
-          alt=""
+          alt=''
           width={200}
           height={150}
-          className="absolute my-auto w-full h-full -z-10"
+          className='absolute my-auto w-full h-full -z-10'
         />
         <Image
-          src="/images/CREATE/dice.svg"
-          alt=""
+          src='/images/CREATE/dice.svg'
+          alt=''
           width={36}
           height={36}
-          className="mr-4 my-auto"
+          className='mr-4 my-auto'
         />
-        <div className="flex justify-center items-center text-4xl text-white">
+        <div className='flex justify-center items-center text-4xl text-white'>
           {imageResult ? (
-            <p className="w-fit py-1 px-6 cursor-pointer">REROLL</p>
+            <p className='w-fit py-1 px-6 cursor-pointer'>REROLL</p>
           ) : imageProcessing ? (
-            <p className="w-fit py-1 px-6 cursor-not-allowed">
+            <p className='w-fit py-1 px-6 cursor-not-allowed'>
               images loading...
             </p>
           ) : pdfData ? (
-            <p className="w-fit py-1 px-6 cursor-pointer">Generate</p>
+            <p className='w-fit py-1 px-6 cursor-pointer'>Generate</p>
           ) : (
-            <p className="w-fit py-1 px-6 cursor-not-allowed">Generate</p>
+            <p className='w-fit py-1 px-6 cursor-not-allowed'>Generate</p>
           )}
         </div>
       </div>
       {imageResult ? (
-        <p className="w-[250px] text-center bg-black bg-opacity-30 rounded-lg p-2 m-2">
-          Regenerate your avatar with the EXACT same prompt or{" "}
+        <p className='w-[250px] text-center bg-black bg-opacity-30 rounded-lg p-2 m-2'>
+          Regenerate your avatar with the EXACT same prompt or{' '}
           <b> make changes below in ADVANCED </b> and then REROLL.
         </p>
       ) : (
-        <p className="m-0">Character Images</p>
+        <p className='m-0'>Character Images</p>
       )}
 
       {imageResult ? (
         <p
-          onClick={() => router.reload("/")}
-          className="my-24 cursor-pointer bg-black bg-opacity-30 rounded-lg p-5"
+          onClick={() => router.reload('/')}
+          className='my-24 cursor-pointer bg-black bg-opacity-30 rounded-lg p-5'
         >
           &#60;&#60; BACK TO START
         </p>
